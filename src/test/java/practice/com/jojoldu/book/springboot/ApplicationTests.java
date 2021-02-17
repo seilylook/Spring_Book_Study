9f993b74240e470c3dd1d24b40783ad158ccd4fe
seilylook@naver.com <seilylook@naver.com>
2021-02-17 13:44:03 +0900

    1 commit

---
+++ src/test/java/practice/com/jojoldu/book/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.com.jojoldu.book.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
