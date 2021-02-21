8c4d55c3c16e633e3e404ff1ffd15f056e8af38b
seilylook@naver.com <seilylook@naver.com>
2021-02-21 18:15:02 +0900

    11 commit

--- src/main/java/practice/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/practice/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,6 +1,20 @@
 package practice.com.jojoldu.book.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import practice.com.jojoldu.book.springboot.service.posts.PostsService;
+import practice.com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
 
-    private
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
 }
