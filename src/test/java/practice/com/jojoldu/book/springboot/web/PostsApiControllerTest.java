ad53b48b1f5bd3eacd0b3ffe1708c4d937c8c6c7
seilylook@naver.com <seilylook@naver.com>
2021-02-22 13:52:32 +0900

    13 commit

---
+++ src/test/java/practice/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,29 @@
+package practice.com.jojoldu.book.springboot.web;
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import practice.com.jojoldu.book.springboot.domain.posts.PostsRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+}
